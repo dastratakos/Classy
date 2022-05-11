c98d7bc6edccdb048a0ee71d99e12750ca5cc754
Dean Stratakos <dastratakos@gmail.com>
2022-05-11 11:30:10 -0700

    Sort home data by startInfo

--- Classy/screens/Home.tsx
+++ Classy/screens/Home.tsx
@@ -77,6 +77,7 @@ export default function Home() {
         }
       }
     }
+    homeDataArr.sort((a, b) => a.startInfo > b.startInfo);
     setHomeData(homeDataArr);
 
     // let dummyDataArr = [];
