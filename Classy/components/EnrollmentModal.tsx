ad331ca73f8ca639471a05718bdf303c4ffe0d39
Grace Alwan <grace.alwan@gmail.com>
2022-05-31 12:47:27 -0700

    abbreivate and style enrollment modal time lists

--- Classy/components/EnrollmentModal.tsx
+++ Classy/components/EnrollmentModal.tsx
@@ -51,8 +51,13 @@ export default function EnrollmentModal({
     ) {
       schedules.splice(j, 1);
       j--;
+    } else {
+      for (let k = 0; k < sched.days.length; k++) {
+        sched.days[k] = sched.days[k].substring(0, 3);
+      }
     }
   }
+
   return (
     <Modal isVisible={visible} onBackdropPress={() => setVisible(false)}>
       <View
@@ -94,19 +99,37 @@ export default function EnrollmentModal({
             style={{ width: "100%", maxHeight: 200 }}
             keyExtractor={(_, index) => index.toString()}
             renderItem={({ item }) => (
-              <Text style={styles.schedText}>
-                {item.days.join(", ")} {getTimeString(item.startInfo)} -{" "}
-                {getTimeString(item.endInfo)}
-              </Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+                
+              >
+                <Text style={styles.schedText}>{item.days.join(", ")}</Text>
+                <Text style={styles.schedText}>
+                  {getTimeString(item.startInfo)} -{" "}
+                  {getTimeString(item.endInfo)}
+                </Text>
+              </View>
             )}
           />
         ) : (
           <View style={styles.classTimesWrap}>
             {schedules.map((schedule, i) => (
-              <Text style={styles.schedText} key={i}>
-                {schedule.days.join(", ")} {getTimeString(schedule.startInfo)} -{" "}
-                {getTimeString(schedule.endInfo)}
-              </Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+                key={i}
+              >
+                <Text style={styles.schedText}>{schedule.days.join(", ")}</Text>
+                <Text style={styles.schedText}>
+                  {getTimeString(schedule.startInfo)} -{" "}
+                  {getTimeString(schedule.endInfo)}
+                </Text>
+              </View>
             ))}
           </View>
         )}
