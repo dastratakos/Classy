25c478b57a9480a53790ecc08250fae6a4de49bf
Dean Stratakos <dastratakos@gmail.com>
2022-06-04 14:14:29 -0700

    Add Mutual and All friends

--- Classy/screens/Friends.tsx
+++ Classy/screens/Friends.tsx
@@ -2,7 +2,7 @@ import { FlatList, StyleSheet } from "react-native";
 import { FriendsProps, User } from "../types";
 import { useContext, useEffect, useState } from "react";
 
-import { ActivityIndicator } from "../components/Themed";
+import { ActivityIndicator, View } from "../components/Themed";
 import AppContext from "../context/Context";
 import AppStyles from "../styles/AppStyles";
 import Colors from "../constants/Colors";
@@ -11,6 +11,8 @@ import FriendCard from "../components/Cards/FriendCard";
 import SVGNoFriends from "../assets/images/undraw/noFriends.svg";
 import { getFriendsFromIds } from "../services/friends";
 import useColorScheme from "../hooks/useColorScheme";
+import TabView from "../components/TabView";
+import Layout from "../constants/Layout";
 
 export default function Friends({ route }: FriendsProps) {
   const context = useContext(AppContext);
@@ -31,25 +33,72 @@ export default function Friends({ route }: FriendsProps) {
 
   if (isLoading) return <ActivityIndicator />;
 
-  return (
-    <FlatList
-      data={friends}
-      keyExtractor={(item) => item.id}
-      renderItem={({ item }) => (
-        <FriendCard
-          friend={item}
-          showFriendStatus={route.params.id !== context.user.id}
+  if (route.params.id === context.user.id)
+    return (
+      <FlatList
+        data={friends}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <FriendCard friend={item} />}
+        contentContainerStyle={{
+          ...AppStyles.section,
+          backgroundColor: Colors[colorScheme].background,
+        }}
+        style={{ backgroundColor: Colors[colorScheme].background }}
+        ListEmptyComponent={
+          <EmptyList SVGElement={SVGNoFriends} primaryText="No friends yet" />
+        }
+      />
+    );
+
+  const tabs = [
+    {
+      label: "All",
+      component: (
+        <FlatList
+          data={friends}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <FriendCard friend={item} showFriendStatus />
+          )}
+          contentContainerStyle={{
+            ...AppStyles.section,
+            backgroundColor: Colors[colorScheme].background,
+          }}
+          style={{ backgroundColor: Colors[colorScheme].background }}
+          ListEmptyComponent={
+            <EmptyList SVGElement={SVGNoFriends} primaryText="No friends yet" />
+          }
         />
-      )}
-      contentContainerStyle={{
-        ...AppStyles.section,
-        backgroundColor: Colors[colorScheme].background,
-      }}
-      style={{ backgroundColor: Colors[colorScheme].background }}
-      ListEmptyComponent={
-        <EmptyList SVGElement={SVGNoFriends} primaryText="No friends yet" />
-      }
-    />
+      ),
+    },
+    {
+      label: "Mutual",
+      component: (
+        <FlatList
+          data={friends.filter((f: User) => context.friendIds.includes(f.id))}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <FriendCard friend={item} />}
+          contentContainerStyle={{
+            ...AppStyles.section,
+            backgroundColor: Colors[colorScheme].background,
+          }}
+          style={{ backgroundColor: Colors[colorScheme].background }}
+          ListEmptyComponent={
+            <EmptyList SVGElement={SVGNoFriends} primaryText="No friends yet" />
+          }
+        />
+      ),
+    },
+  ];
+
+  return (
+    <View style={{ flex: 1, paddingTop: Layout.spacing.medium }}>
+      <TabView
+        tabs={tabs}
+        addTabMargin
+        selectedStyle={{ backgroundColor: Colors.pink }}
+      />
+    </View>
   );
 }
 
