7c62237f490b3b4c504f69a79ae4c60fb796ce14
Dean Stratakos <dastratakos@gmail.com>
2022-06-03 21:57:22 -0700

    Format majors

--- Classy/utils/majorList.ts
+++ Classy/utils/majorList.ts
@@ -7,43 +7,118 @@ export const majorList = [
     label: "African and African American Studies",
     value: "African and African American Studies",
   },
-  { label: "African Studies", value: "African Studies" },
-  { label: "American Studies", value: "American Studies" },
-  { label: "Anthropology", value: "Anthropology" },
+  {
+    label: "African Studies",
+    value: "African Studies",
+  },
+  {
+    label: "American Studies",
+    value: "American Studies",
+  },
+  {
+    label: "Anthropology",
+    value: "Anthropology",
+  },
   {
     label: "Applied and Engineering Physics",
     value: "Applied and Engineering Physics",
   },
-  { label: "Archaeology", value: "Archaeology" },
-  { label: "Architectural Design", value: "Architectural Design" },
-  { label: "Art History", value: "Art History" },
-  { label: "Art Practice", value: "Art Practice" },
-  { label: "Asian American Studies", value: "Asian American Studies" },
-  { label: "Atmosphere/Energy", value: "Atmosphere/Energy" },
-  { label: "Biochemistry ", value: "Biochemistry " },
-  { label: "Bioengineering", value: "Bioengineering" },
-  { label: "Biology", value: "Biology" },
-  { label: "Biomechanical Engineering", value: "Biomechanical Engineering" },
-  { label: "Biomedical Computation", value: "Biomedical Computation" },
-  { label: "Biomedical Informatics", value: "Biomedical Informatics" },
-  { label: "Biomedical Physics", value: "Biomedical Physics" },
-  { label: "Biophysics ", value: "Biophysics " },
-  { label: "Business", value: "Business" },
-  { label: "Cancer Biology", value: "Cancer Biology" },
+  {
+    label: "Archaeology",
+    value: "Archaeology",
+  },
+  {
+    label: "Architectural Design",
+    value: "Architectural Design",
+  },
+  {
+    label: "Art History",
+    value: "Art History",
+  },
+  {
+    label: "Art Practice",
+    value: "Art Practice",
+  },
+  {
+    label: "Asian American Studies",
+    value: "Asian American Studies",
+  },
+  {
+    label: "Atmosphere/Energy",
+    value: "Atmosphere/Energy",
+  },
+  {
+    label: "Biochemistry ",
+    value: "Biochemistry ",
+  },
+  {
+    label: "Bioengineering",
+    value: "Bioengineering",
+  },
+  {
+    label: "Biology",
+    value: "Biology",
+  },
+  {
+    label: "Biomechanical Engineering",
+    value: "Biomechanical Engineering",
+  },
+  {
+    label: "Biomedical Computation",
+    value: "Biomedical Computation",
+  },
+  {
+    label: "Biomedical Informatics",
+    value: "Biomedical Informatics",
+  },
+  {
+    label: "Biomedical Physics",
+    value: "Biomedical Physics",
+  },
+  {
+    label: "Biophysics ",
+    value: "Biophysics ",
+  },
+  {
+    label: "Business",
+    value: "Business",
+  },
+  {
+    label: "Cancer Biology",
+    value: "Cancer Biology",
+  },
   {
     label: "Chemical and Systems Biology",
     value: "Chemical and Systems Biology",
   },
-  { label: "Chemical Engineering", value: "Chemical Engineering" },
-  { label: "Chemistry", value: "Chemistry" },
-  { label: "Chicana/o Latino/a Studies", value: "Chicana/o Latino/a Studies" },
-  { label: "China Studies", value: "China Studies" },
+  {
+    label: "Chemical Engineering",
+    value: "Chemical Engineering",
+  },
+  {
+    label: "Chemistry",
+    value: "Chemistry",
+  },
+  {
+    label: "Chicana/o Latino/a Studies",
+    value: "Chicana/o Latino/a Studies",
+  },
+  {
+    label: "China Studies",
+    value: "China Studies",
+  },
   {
     label: "Civil and Environmental Engineering",
     value: "Civil and Environmental Engineering",
   },
-  { label: "Classics", value: "Classics" },
-  { label: "Communication", value: "Communication" },
+  {
+    label: "Classics",
+    value: "Classics",
+  },
+  {
+    label: "Communication",
+    value: "Communication",
+  },
   {
     label: "Community Health and Preventative Research",
     value: "Community Health and Preventative Research",
@@ -52,7 +127,10 @@ export const majorList = [
     label: "Community Health and Prevention Research",
     value: "Community Health and Prevention Research",
   },
-  { label: "Comparative Literature", value: "Comparative Literature" },
+  {
+    label: "Comparative Literature",
+    value: "Comparative Literature",
+  },
   {
     label: "Comparative Studies in Race and Ethnicity",
     value: "Comparative Studies in Race and Ethnicity",
@@ -61,33 +139,78 @@ export const majorList = [
     label: "Computational and Mathematical Engineering",
     value: "Computational and Mathematical Engineering",
   },
-  { label: "Computer Science", value: "Computer Science" },
-  { label: "Creative Writing", value: "Creative Writing" },
+  {
+    label: "Computer Science",
+    value: "Computer Science",
+  },
+  {
+    label: "Creative Writing",
+    value: "Creative Writing",
+  },
   {
     label: "Curriculum Studies and Teacher Education",
     value: "Curriculum Studies and Teacher Education",
   },
-  { label: "Dance (TAPS Minor)", value: "Dance (TAPS Minor)" },
+  {
+    label: "Dance (TAPS Minor)",
+    value: "Dance (TAPS Minor)",
+  },
   {
     label: "Democracy, Development, and the Rule of Law",
     value: "Democracy, Development, and the Rule of Law",
   },
-  { label: "Design Impact ", value: "Design Impact " },
-  { label: "Developmental Biology", value: "Developmental Biology" },
-  { label: "Digital Humanities", value: "Digital Humanities" },
-  { label: "Documentary Film and Video", value: "Documentary Film and Video" },
-  { label: "Earth Systems", value: "Earth Systems" },
-  { label: "East Asian Studies", value: "East Asian Studies" },
-  { label: "Economics", value: "Economics" },
-  { label: "Education", value: "Education" },
-  { label: "Education Data Science", value: "Education Data Science" },
-  { label: "Electrical Engineering", value: "Electrical Engineering" },
+  {
+    label: "Design Impact ",
+    value: "Design Impact ",
+  },
+  {
+    label: "Developmental Biology",
+    value: "Developmental Biology",
+  },
+  {
+    label: "Digital Humanities",
+    value: "Digital Humanities",
+  },
+  {
+    label: "Documentary Film and Video",
+    value: "Documentary Film and Video",
+  },
+  {
+    label: "Earth Systems",
+    value: "Earth Systems",
+  },
+  {
+    label: "East Asian Studies",
+    value: "East Asian Studies",
+  },
+  {
+    label: "Economics",
+    value: "Economics",
+  },
+  {
+    label: "Education",
+    value: "Education",
+  },
+  {
+    label: "Education Data Science",
+    value: "Education Data Science",
+  },
+  {
+    label: "Electrical Engineering",
+    value: "Electrical Engineering",
+  },
   {
     label: "Energy Resources Engineering",
     value: "Energy Resources Engineering",
   },
-  { label: "Engineering Physics", value: "Engineering Physics" },
-  { label: "English", value: "English" },
+  {
+    label: "Engineering Physics",
+    value: "Engineering Physics",
+  },
+  {
+    label: "English",
+    value: "English",
+  },
   {
     label: "Environmental Systems Engineering",
     value: "Environmental Systems Engineering",
@@ -96,33 +219,78 @@ export const majorList = [
     label: "Epidemiology and Clinical Research",
     value: "Epidemiology and Clinical Research",
   },
-  { label: "Ethics in Society", value: "Ethics in Society" },
-  { label: "European Studies", value: "European Studies" },
+  {
+    label: "Ethics in Society",
+    value: "Ethics in Society",
+  },
+  {
+    label: "European Studies",
+    value: "European Studies",
+  },
   {
     label: "Feminist, Gender, and Sexuality Studies",
     value: "Feminist, Gender, and Sexuality Studies",
   },
-  { label: "Film and Media Studies", value: "Film and Media Studies" },
-  { label: "French", value: "French" },
-  { label: "Genetics ", value: "Genetics " },
-  { label: "Geological Sciences", value: "Geological Sciences" },
-  { label: "Geophysics ", value: "Geophysics " },
-  { label: "German Studies", value: "German Studies" },
-  { label: "Global Studies", value: "Global Studies" },
-  { label: "Health Policy", value: "Health Policy" },
-  { label: "History", value: "History" },
-  { label: "Honors in the Arts", value: "Honors in the Arts" },
-  { label: "Human Biology", value: "Human Biology" },
+  {
+    label: "Film and Media Studies",
+    value: "Film and Media Studies",
+  },
+  {
+    label: "French",
+    value: "French",
+  },
+  {
+    label: "Genetics ",
+    value: "Genetics ",
+  },
+  {
+    label: "Geological Sciences",
+    value: "Geological Sciences",
+  },
+  {
+    label: "Geophysics ",
+    value: "Geophysics ",
+  },
+  {
+    label: "German Studies",
+    value: "German Studies",
+  },
+  {
+    label: "Global Studies",
+    value: "Global Studies",
+  },
+  {
+    label: "Health Policy",
+    value: "Health Policy",
+  },
+  {
+    label: "History",
+    value: "History",
+  },
+  {
+    label: "Honors in the Arts",
+    value: "Honors in the Arts",
+  },
+  {
+    label: "Human Biology",
+    value: "Human Biology",
+  },
   {
     label: "Human Genetics and Gentic Counseling",
     value: "Human Genetics and Gentic Counseling",
   },
-  { label: "Human Rights", value: "Human Rights" },
+  {
+    label: "Human Rights",
+    value: "Human Rights",
+  },
   {
     label: "Iberian & Latin American Cultures",
     value: "Iberian & Latin American Cultures",
   },
-  { label: "Immunology ", value: "Immunology " },
+  {
+    label: "Immunology ",
+    value: "Immunology ",
+  },
   {
     label: "International Comparative Education",
     value: "International Comparative Education",
@@ -131,25 +299,58 @@ export const majorList = [
     label: "International Policy Studies",
     value: "International Policy Studies",
   },
-  { label: "International Relations", value: "International Relations" },
+  {
+    label: "International Relations",
+    value: "International Relations",
+  },
   {
     label: "International Security Studies",
     value: "International Security Studies",
   },
-  { label: "Iranian Studies", value: "Iranian Studies" },
-  { label: "Islamic Studies", value: "Islamic Studies" },
-  { label: "Italian", value: "Italian" },
-  { label: "Japanese", value: "Japanese" },
-  { label: "Jewish Studies", value: "Jewish Studies" },
-  { label: "Korean", value: "Korean" },
-  { label: "Laboratory Animal Science", value: "Laboratory Animal Science" },
-  { label: "Latin American Studies", value: "Latin American Studies" },
-  { label: "Law", value: "Law" },
+  {
+    label: "Iranian Studies",
+    value: "Iranian Studies",
+  },
+  {
+    label: "Islamic Studies",
+    value: "Islamic Studies",
+  },
+  {
+    label: "Italian",
+    value: "Italian",
+  },
+  {
+    label: "Japanese",
+    value: "Japanese",
+  },
+  {
+    label: "Jewish Studies",
+    value: "Jewish Studies",
+  },
+  {
+    label: "Korean",
+    value: "Korean",
+  },
+  {
+    label: "Laboratory Animal Science",
+    value: "Laboratory Animal Science",
+  },
+  {
+    label: "Latin American Studies",
+    value: "Latin American Studies",
+  },
+  {
+    label: "Law",
+    value: "Law",
+  },
   {
     label: "Learning Design and Technology",
     value: "Learning Design and Technology",
   },
-  { label: "Linguistics", value: "Linguistics" },
+  {
+    label: "Linguistics",
+    value: "Linguistics",
+  },
   {
     label: "Management Science and Engineering",
     value: "Management Science and Engineering",
@@ -162,10 +363,22 @@ export const majorList = [
     label: "Mathematical and Computational Science",
     value: "Mathematical and Computational Science",
   },
-  { label: "Mathematics", value: "Mathematics" },
-  { label: "Mechanical Engineering", value: "Mechanical Engineering" },
-  { label: "Medicine MD Program", value: "Medicine MD Program" },
-  { label: "Medieval Studies", value: "Medieval Studies" },
+  {
+    label: "Mathematics",
+    value: "Mathematics",
+  },
+  {
+    label: "Mechanical Engineering",
+    value: "Mechanical Engineering",
+  },
+  {
+    label: "Medicine MD Program",
+    value: "Medicine MD Program",
+  },
+  {
+    label: "Medieval Studies",
+    value: "Medieval Studies",
+  },
   {
     label: "Microbiology and Immunology",
     value: "Microbiology and Immunology",
@@ -174,7 +387,10 @@ export const majorList = [
     label: "Middle Eastern Language, Literature, and Culture",
     value: "Middle Eastern Language, Literature, and Culture",
   },
-  { label: "Modern Language", value: "Modern Language" },
+  {
+    label: "Modern Language",
+    value: "Modern Language",
+  },
   {
     label: "Modern Thought and Literature",
     value: "Modern Thought and Literature",
@@ -183,14 +399,26 @@ export const majorList = [
     label: "Molecular and Cellular Physiology ",
     value: "Molecular and Cellular Physiology ",
   },
-  { label: "Music", value: "Music" },
+  {
+    label: "Music",
+    value: "Music",
+  },
   {
     label: "Music, Science, and Technology",
     value: "Music, Science, and Technology",
   },
-  { label: "Native American Studies", value: "Native American Studies" },
-  { label: "Neurosciences ", value: "Neurosciences " },
-  { label: "Philosophy", value: "Philosophy" },
+  {
+    label: "Native American Studies",
+    value: "Native American Studies",
+  },
+  {
+    label: "Neurosciences ",
+    value: "Neurosciences ",
+  },
+  {
+    label: "Philosophy",
+    value: "Philosophy",
+  },
   {
     label: "Philosophy and Religious Studies",
     value: "Philosophy and Religious Studies",
@@ -199,17 +427,38 @@ export const majorList = [
     label: "Physician Assistant Studies",
     value: "Physician Assistant Studies",
   },
-  { label: "Physics", value: "Physics" },
+  {
+    label: "Physics",
+    value: "Physics",
+  },
   {
     label: "Policy Organization and Leadership Studies",
     value: "Policy Organization and Leadership Studies",
   },
-  { label: "Political Science", value: "Political Science" },
-  { label: "Portuguese", value: "Portuguese" },
-  { label: "Product Design", value: "Product Design" },
-  { label: "Psychology", value: "Psychology" },
-  { label: "Public Policy", value: "Public Policy" },
-  { label: "Religious Studies", value: "Religious Studies" },
+  {
+    label: "Political Science",
+    value: "Political Science",
+  },
+  {
+    label: "Portuguese",
+    value: "Portuguese",
+  },
+  {
+    label: "Product Design",
+    value: "Product Design",
+  },
+  {
+    label: "Psychology",
+    value: "Psychology",
+  },
+  {
+    label: "Public Policy",
+    value: "Public Policy",
+  },
+  {
+    label: "Religious Studies",
+    value: "Religious Studies",
+  },
   {
     label: "Science, Technology, and Society",
     value: "Science, Technology, and Society",
@@ -224,34 +473,82 @@ export const majorList = [
     value:
       "Social Sciences, Humanities, and Interdisciplinary Policy Studies in Education",
   },
-  { label: "Sociology", value: "Sociology" },
-  { label: "South Asian Studies", value: "South Asian Studies" },
-  { label: "Spanish", value: "Spanish" },
+  {
+    label: "Sociology",
+    value: "Sociology",
+  },
+  {
+    label: "South Asian Studies",
+    value: "South Asian Studies",
+  },
+  {
+    label: "Spanish",
+    value: "Spanish",
+  },
   {
     label: "Stanford Teacher Education Program",
     value: "Stanford Teacher Education Program",
   },
-  { label: "Statistics", value: "Statistics" },
+  {
+    label: "Statistics",
+    value: "Statistics",
+  },
   {
     label: "Stem Cell Biology and Regenerative Medicine",
     value: "Stem Cell Biology and Regenerative Medicine",
   },
-  { label: "Structural Biology", value: "Structural Biology" },
-  { label: "Sustainability", value: "Sustainability" },
-  { label: "Symbolic Systems", value: "Symbolic Systems" },
+  {
+    label: "Structural Biology",
+    value: "Structural Biology",
+  },
+  {
+    label: "Sustainability",
+    value: "Sustainability",
+  },
+  {
+    label: "Symbolic Systems",
+    value: "Symbolic Systems",
+  },
   {
     label: "Theater and Performance Studies",
     value: "Theater and Performance Studies",
   },
-  { label: "Translation Studies", value: "Translation Studies" },
-  { label: "Urban Studies", value: "Urban Studies" },
-  { label: "First Year", value: "First Year" },
-  { label: "Second Year", value: "Second Year" },
-  { label: "Third Year", value: "Third Year" },
-  { label: "Fourth Year", value: "Fourth Year" },
-  { label: "1L", value: "1L" },
-  { label: "2L", value: "2L" },
-  { label: "3L", value: "3L" },
+  {
+    label: "Translation Studies",
+    value: "Translation Studies",
+  },
+  {
+    label: "Urban Studies",
+    value: "Urban Studies",
+  },
+  {
+    label: "First Year",
+    value: "First Year",
+  },
+  {
+    label: "Second Year",
+    value: "Second Year",
+  },
+  {
+    label: "Third Year",
+    value: "Third Year",
+  },
+  {
+    label: "Fourth Year",
+    value: "Fourth Year",
+  },
+  {
+    label: "1L",
+    value: "1L",
+  },
+  {
+    label: "2L",
+    value: "2L",
+  },
+  {
+    label: "3L",
+    value: "3L",
+  },
 ];
 
 /** All Majors
